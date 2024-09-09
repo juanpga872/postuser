bc046ddf1b8bcc17740418d03a02f164092cd248
juan pablo garces <juanpagacevedo@gmail.com>
2024-09-09 07:52:38 -0500

    frist commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import React from 'react';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )}
